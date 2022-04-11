25adce16af2646fec2e81613f5ad84e2ddba94bd
AkagiYuu <khanghuynh0245@gmail.com>
2022-04-11 16:28:34 +0700

    Minor fix

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,19 +1,26 @@
-require('kanagawa').setup({
-    commentStyle = "NONE",
-    functionStyle = "NONE",
-    keywordStyle = "italic",
-    statementStyle = "bold",
-    typeStyle = "NONE",
-    variablebuiltinStyle = "italic",
-    specialReturn = true,       -- special highlight for the return keyword
-    specialException = true,    -- special highlight for exception handling keywords
-    transparent = true,        -- do not set background color
-    overrides = {
-        CursorLine = { bg = "NONE" },
-        TelescopeBorder = { bg = "NONE" }
-    },
-})
-pcall(vim.cmd, "colorscheme kanagawa")
+local ok, kanagawa = pcall(require, "kanagawa")
+if ok then
+    kanagawa.setup({
+        commentStyle = "NONE",
+        functionStyle = "NONE",
+        keywordStyle = "italic",
+        statementStyle = "bold",
+        typeStyle = "NONE",
+        variablebuiltinStyle = "italic",
+        specialReturn = true, -- special highlight for the return keyword
+        specialException = true, -- special highlight for exception handling keywords
+        transparent = true, -- do not set background color
+        overrides = {
+            CursorLine = {
+                bg = "NONE"
+            },
+            TelescopeBorder = {
+                bg = "NONE"
+            }
+        }
+    })
+    vim.cmd("colorscheme kanagawa")
+end
 -- local catppuccin = require("catppuccin")
 -- catppuccin.setup({
 --     transparent_background = true,
