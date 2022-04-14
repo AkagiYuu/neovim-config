6758ea28e60dfde27deba560df719ca697045166
AkagiYuu <khanghuynh0245@gmail.com>
2022-04-14 23:15:36 +0700

    nvimtree indent markers

--- lua/plugins/config/nvim-tree.lua
+++ lua/plugins/config/nvim-tree.lua
@@ -1,7 +1,16 @@
 local g = vim.g
-g.nvim_tree_indent_markers = 1
 g.nvim_tree_highlight_opened_files = 1
 g.nvim_tree_group_empty = 1
 require("nvim-tree").setup({
-	hijack_cursor = true,
+    hijack_cursor = true,
+    renderer = {
+        indent_markers = {
+            enable = true,
+            icons = {
+                corner = "└ ",
+                edge = "│ ",
+                none = "  "
+            }
+        }
+    }
 })
