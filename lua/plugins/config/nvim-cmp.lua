c2220e8e8ed58a7b29ea259b9142c14fb83f7354
AkagiYuu <khanghuynh0245@gmail.com>
2022-04-14 23:14:53 +0700

    Add cmp key map

--- lua/plugins/config/nvim-cmp.lua
+++ lua/plugins/config/nvim-cmp.lua
@@ -1,84 +1,130 @@
 local cmp = require("cmp")
 local luasnip = require("luasnip")
 local cmp_kinds = {
-	Text = "  ",
-	Method = "  ",
-	Function = "  ",
-	Constructor = "  ",
-	Field = "  ",
-	Variable = "  ",
-	Class = "  ",
-	Interface = "  ",
-	Module = "  ",
-	Property = "  ",
-	Unit = "  ",
-	Value = "  ",
-	Enum = "  ",
-	Keyword = "  ",
-	Snippet = "  ",
-	Color = "  ",
-	File = "  ",
-	Reference = "  ",
-	Folder = "  ",
-	EnumMember = "  ",
-	Constant = "  ",
-	Struct = "  ",
-	Event = "  ",
-	Operator = "  ",
-	TypeParameter = "  ",
+    Text = "  ",
+    Method = "  ",
+    Function = "  ",
+    Constructor = "  ",
+    Field = "  ",
+    Variable = "  ",
+    Class = "  ",
+    Interface = "  ",
+    Module = "  ",
+    Property = "  ",
+    Unit = "  ",
+    Value = "  ",
+    Enum = "  ",
+    Keyword = "  ",
+    Snippet = "  ",
+    Color = "  ",
+    File = "  ",
+    Reference = "  ",
+    Folder = "  ",
+    EnumMember = "  ",
+    Constant = "  ",
+    Struct = "  ",
+    Event = "  ",
+    Operator = "  ",
+    TypeParameter = "  "
 }
 cmp.setup({
-	snippet = {
-		expand = function(args)
-			luasnip.lsp_expand(args.body)
-		end,
-	},
-	mapping = {
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end
+    },
+    mapping = {
+        ['<Down>'] = {
+            i = cmp.mapping.select_next_item({
+                behavior = 'insert'
+            })
+        },
+        ['<Up>'] = {
+            i = cmp.mapping.select_prev_item({
+                behavior = 'insert'
+            })
+        },
+        ['<C-e>'] = {
+            i = cmp.mapping.abort(),
+            c = cmp.mapping.close()
+        },
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-y>"] = {
+            i = cmp.mapping.confirm({
+                select = false
+            })
+        },
+        ["<Tab>"] = {
+			i = function(fallback)
+				if luasnip.expand_or_jumpable() then
+					luasnip.expand_or_jump()
+				else
+					fallback()
+				end
+			end,
+			s = function(fallback)
+				if luasnip.expand_or_jumpable() then
+					luasnip.expand_or_jump()
+				else
+					fallback()
+				end
+			end,
+			c = function(fallback)
+                if cmp.visible() then
+                    cmp.select_next_item()
+                else
+                    fallback()
+                end
+            end
+		},
 
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-	},
-	sources = cmp.config.sources({
-		{ name = "nvim_lsp" },
-		{ name = "luasnip" },
-		{ name = "path" }
-	}, {
-		{ name = "buffer" },
+        ["<S-Tab>"] = {
+            i = function(fallback)
+                if luasnip.jumpable(-1) then
+                    luasnip.jump(-1)
+                else
+                    fallback()
+                end
+            end,
+			s = function(fallback)
+				if luasnip.jumpable(-1) then
+					luasnip.jump(-1)
+				else
+					fallback()
+				end
+			end,
+            c = function(fallback)
+                if cmp.visible() then
+                    cmp.select_prev_item()
+                else
+                    fallback()
+                end
+            end
+        }
+    },
+    sources = cmp.config.sources({
+		{ name = "nvim_lsp" }, { name = "luasnip"}
 	}),
-	formatting = {
-		fields = { "kind", "abbr" },
-		format = function(_, vim_item)
-			vim_item.kind = cmp_kinds[vim_item.kind] or ""
-			return vim_item
-		end,
-	},
+    formatting = {
+        fields = {"kind", "abbr"},
+        format = function(_, vim_item)
+            vim_item.kind = cmp_kinds[vim_item.kind] or ""
+            return vim_item
+        end
+    }
 })
 
 cmp.setup.cmdline("/", {
-	sources = {
-		{ name = "buffer" },
-	}
+    sources = {{
+        name = "buffer"
+    }}
 })
 
 cmp.setup.cmdline(":", {
-	sources = cmp.config.sources({
-		{ name = "path" },
-	}, {
-		{ name = "cmdline" },
-	})
+    sources = cmp.config.sources({{
+        name = "path"
+    }}, {{
+        name = "cmdline"
+    }})
 })
 require("luasnip.loaders.from_vscode").load()
