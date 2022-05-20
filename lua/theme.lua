703ede5a2c446e1322b16e267ca70a7e3adc904e
AkagiYuu <khanghuynh0245@gmail.com>
2022-05-20 11:01:01 +0700

    Minor fix

--- lua/theme.lua
+++ lua/theme.lua
@@ -25,4 +25,4 @@ vim.g.vscode_style = "dark"
 vim.g.vscode_transparent = 1
 -- Disable nvim-tree background color
 vim.g.vscode_disable_nvimtree_bg = true
-vim.cmd([[colorscheme vscode]])
+pcall(vim.cmd,[[colorscheme vscode]])
