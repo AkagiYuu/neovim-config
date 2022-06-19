5e787c801f8d9cb8547a562cd6ac1ccaef8c868c
AkagiYuu <khanghuynh0245@gmail.com>
2022-06-19 14:28:23 +0700

    Minor fix

--- lua/util.lua
+++ lua/util.lua
@@ -31,7 +31,7 @@ M.load = function(modules)
 	for _, module in ipairs(modules) do
 		local ok, err = pcall(require, module)
 		if not ok then
-			error("Error loading " .. module .. "\n\n" .. err)
+			vim.notify("Error loading " .. module .. "\n\n" .. err)
 		end
 	end
 end
